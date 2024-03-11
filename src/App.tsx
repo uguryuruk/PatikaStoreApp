5de2900d7e7ba1c59367f63e377574d5c14e0060
Ugur Y <uguryuruk@yahoo.com>
2024-03-11 20:04:36 +0300

    main style ops

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ import {
 } from 'react-native';
 import React from 'react';
 
+const todoData = [
+  {id: 1, task: 'Temizlik'},
+  {id: 2, task: 'Çamaşır'},
+];
+
 const TodoInput = () => {
   return (
-    <View>
+    <View style={styles.inner_container}>
       <TextInput placeholder="Yapılacak.." />
       <TouchableOpacity style={styles.addButton} onPress={x => console.log(x)}>
-        <Text>Kaydet</Text>
+        <Text style={styles.buttonText}>Kaydet</Text>
       </TouchableOpacity>
     </View>
   );
@@ -20,8 +25,11 @@ const TodoInput = () => {
 
 const App = () => {
   return (
-    <View>
-      <Text style={styles.header}>Yapılacaklar: 2</Text>
+    <View style={styles.container}>
+      <View style={styles.headContainer}>
+        <Text style={styles.header}>Yapılacaklar:</Text>
+        <Text style={styles.header}>2</Text>
+      </View>
 
       <TodoInput />
     </View>
@@ -31,12 +39,34 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#102027',
+    flex: 1,
+  },
+  headContainer:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'space-around'
+  },
   header: {
-    fontSize: 16,
-    color: 'orange',
+    fontSize: 36,
+    color: '#FFA500',
+  },
+  inner_container: {
+    backgroundColor: '#37474F',
   },
   addButton: {
-    backgroundColor: 'orange',
+    backgroundColor: '#FFA500',
+    borderRadius: 5,
+    height: 40,
+    width: 300,
+    margin: 5,
+    padding: 5,
+    alignSelf: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
     color: 'white',
+    textAlign: 'center',
   },
 });
